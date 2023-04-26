eb1c7971a7c35e15fa4fba0c1b83d390f342b5b0
Nikko-Adrian-Pacleb <pacleb.nikko@gmail.com>
2023-04-26 00:53:02 -0700

    Initial Views Commit

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ connectDB();
 
 const app = express();
 
+// Set view engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
